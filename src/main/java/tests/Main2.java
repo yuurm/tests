15244f9543b362538961df121aa2be21c41789bf
yuurm <73988712+yuurm@users.noreply.github.com>
2023-10-27 16:27:08 +0300

    add new Main

--- src/main/java/tests/Main2.java
+++ src/main/java/tests/Main2.java
@@ -7,29 +7,18 @@ import tests.app.Tests;
 import tests.app.Value;
 import tests.app.Values;
 
-
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main2 {
     public static Map<Integer, String> VALUES_MAP = new HashMap<>();
     public static Gson gson;
 
     public static void main(String[] args) {
-        Scanner scanTest = new Scanner(System.in);
-        Scanner scanValue = new Scanner(System.in);
-        Scanner scanReport = new Scanner(System.in);
-        System.out.println("Укажите путь к файлу тестами: ");
-        String testJsonPath = scanTest.nextLine();
-        System.out.println("Укажите путь к файлу со значениями: ");
-        String valueJsonPath = scanValue.nextLine();
-        System.out.println("Укажите путь, куда сохранить файл: ");
-        String reportJsonPath = scanReport.nextLine() + "/report2.json"; // в зависимости от ОС
+        // Жестко заданные пути к файлам
+        String testJsonPath = "tests.json";
+        String valueJsonPath = "values.json";
+        String reportJsonPath = "reports/report.json";
 
         try (Reader testsReader = new FileReader(testJsonPath);
              Reader valuesReader = new FileReader(valueJsonPath);
@@ -54,9 +43,8 @@ public class Main {
         }
     }
 
-    public static void setValues (Test test) {
-
-        for (Integer id : Main.VALUES_MAP.keySet()){
+    public static void setValues(Test test) {
+        for (Integer id : Main.VALUES_MAP.keySet()) {
             if (test.getId() == id) {
                 test.setValue(Main.VALUES_MAP.get(id));
             }
@@ -67,16 +55,4 @@ public class Main {
             }
         }
     }
-
-    private static String getJsonFile(String jsonPath) {
-        StringBuilder builder = new StringBuilder();
-        try {
-            List<String> lines = Files.readAllLines(Paths.get(jsonPath));
-            lines.forEach(builder::append);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return builder.toString();
-    }
-
 }
